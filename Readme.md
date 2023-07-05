b6eb226ae2cf966572404558d9e8fbf50a874924
ivangotovets <gotovets@gmail.com>
2023-07-05 19:43:06 +0300

    fb2 parser done

---
+++ Readme.md
@@ -0,0 +1,28 @@
+## Как запустить проект
+### Клонировать репозиторий и перейти в него в командной строке:
+
+`git clone git@github.com:ivangotovets/weborama_test.git`
+
+`cd Weborama_test`
+
+### Cоздать и активировать виртуальное окружение:
+
+`python3 -m venv venv`
+
+`source venv/bin/activate`
+
+### Установить зависимости из файла requirements.txt:
+
+`python -m pip install --upgrade pip`
+
+`pip install -r requirements.txt`
+
+### Задание 1 - файл Jupiter Notebook "task_1.ipynb"
+
+### Запуск парсера
+
+`python parse.py TRANSHUMANISM.fb2`
+`python parse.py TRANSHUMANISM.epub`
+`python parse.py КГБТ.fb2`
+`python parse.py КГБТ.epub`
+
--- parse.py
+++ parse.py
@@ -1,22 +1,70 @@
+import json
 import sys
 import warnings
+import xmltodict
 
+# пакет сразу под капотом преобразует epub в json
 from ebooklib import epub
 
+# перед парсингом открыть подходящим редактором файлы
+# для того, чтобы понять, где искать метадату и сделать мэппинг ключей
 EPUB_MAP = {
-    'Author':  'creator',
-    'Title': 'title',
-    'Published by': 'publisher',
-    'Date': 'date'
+    "Author": "creator",
+    "Title": "title",
+    "Published by": "publisher",
+    "Year": "date",
 }
 
-warnings.filterwarnings("ignore")
-book_name = sys.argv[1]
-book = epub.read_epub(book_name)
-md = book.metadata['http://purl.org/dc/elements/1.1/']
+FB2_MAP = {
+    "Author": "sequence",
+    "Title": "book-name",
+    "Published by": "publisher",
+    "Year": "year",
+}
 
-if book_name.endswith('.epub'):
-    for key, value in EPUB_MAP.items():
-        print('{}: {}'.format(key, md[value][0][0]))
+
+warnings.filterwarnings("ignore")
+
+# достать название файла из аргумента CLI
+book_name = sys.argv[1]
+
+if book_name.endswith(".epub"):
+    # сконвертировать epub в json
+    book = epub.read_epub(book_name)
+    # достать метадату по ключу, если не нашли - перехватить исключение
+    try:
+        meta = book.metadata["http://purl.org/dc/elements/1.1/"]
+    except KeyError:
+        print("Метадата о книге отсутствует")
+    else:
+        # вывести данные согласно мэппингу
+        for key, value in EPUB_MAP.items():
+            print("{}: {}".format(key, meta[value][0][0]))
+
+elif book_name.endswith(".fb2"):
+    with (
+        # считать файл как xml побайтно
+        open(book_name, "rb") as file_in,
+        # метадату удобно анализировать в json в отдельном файле
+        # оставил как пример
+        open("output.json", "w", encoding="UTF-8") as file_out,
+    ):
+        lines = file_in.read()
+        # сконвертировать xml в json
+        data = xmltodict.parse(lines)
+        try:
+            # достать метадату, если не нашли - перехватить исключение
+            meta = data["FictionBook"]["description"]["publish-info"]
+        except KeyError:
+            print("Метадата о книге отсутствует")
+        else:
+            # анализировать формат вывода метадаты
+            json.dump(data, file_out, ensure_ascii=False, indent=4)
+            for key, value in FB2_MAP.items():
+                if value == "sequence":
+                    # достать автора по отдельному ключу в словаре 'sequence'
+                    print("{}: {}".format(key, meta[value]["@name"]))
+                else:
+                    print("{}: {}".format(key, meta[value]))
 else:
-    print('Неверный формат файла')
+    print("Неверный формат файла")
